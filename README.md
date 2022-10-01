74d61c708e707093db993f820a4d5a9c91a1265b
EvgenyM <MelnikovEI@ya.ru>
2022-10-01 20:45:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Get weather
+
+The script does:
+1. If you enter any url, it converts it to short link (using service www.bitly.com).
+2. If you enter a short link, it displays a number of clicks for this link for all the time.
+
+## Getting Started
+Just run the script "python main.py"
+
+```
+python main.py
+```
+### Prerequisites
+
+You need to install the python interpreter, "requests", "python-dotenv" modules
+
+```
+pip install requests
+pip install python-dotenv
+```
+### Usage
+1
+
+Enter bitlink or any url
+
+https://yandex.ru/pogoda/nizhny-novgorod?lat=56.236426&lon=43.846158
+
+Bitlink for your url:  https://bit.ly/3LXoM09
+
+2
+
+Enter bitlink or any url
+
+https://bit.ly/3LXoM09
+
+The number of clicks is  8
+## Authors
+
+* **Evgeny Melnikov** - *Initial work* - [Evgeny Melnikov](https://github.com/MelnikovEI)
+
+## Acknowledgments
+
+* Inspired by [Devman](https://dvmn.org/)
---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import requests
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
+def shorten_link(token, url):
+    shorten_url = 'https://api-ssl.bitly.com/v4/shorten'
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'Content-Type': 'application/json',
+    }
+    body = {"long_url": url}
+    response_post = requests.post(shorten_url, headers=headers, json=body)
+    response_post.raise_for_status()
+    return response_post.json()['link']
+
+
+def count_clicks(token, bitlink):
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'Content-Type': 'application/json',
+    }
+    url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
+    parsed = urlparse(bitlink)
+    link_in_bitlink_format = (parsed.hostname if parsed.hostname else '') + parsed.path
+    url = url_template.format(link_in_bitlink_format)
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(token, url):
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'Content-Type': 'application/json',
+    }
+    url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
+    parsed = urlparse(url)
+    link_in_bitlink_format = (parsed.hostname if parsed.hostname else '') + parsed.path
+    url_get_bitlink_info = url_template.format(link_in_bitlink_format)
+    response = requests.get(url_get_bitlink_info, headers=headers)
+    return response.ok
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    my_token = os.environ['TOKEN']
+    try:
+        input_link = input('Enter bitlink or any url\n')
+        if is_bitlink(my_token, input_link):
+            print('The number of clicks is ', count_clicks(my_token, input_link))
+        else:
+            print('Bitlink for your url: ', shorten_link(my_token, input_link))
+    except requests.exceptions.HTTPError:
+        print('Entered url is not valid!')
