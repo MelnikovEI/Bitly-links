39cb1b7d58c69789f239911a5430d3da84e98135
EvgenyM <MelnikovEI@ya.ru>
2022-10-03 20:04:32 +0300

    Excluded prefix "my_" to improve style

--- main.py
+++ main.py
@@ -36,12 +36,12 @@ def is_bitlink(token, url):
 
 if __name__ == '__main__':
     load_dotenv()
-    my_token = os.environ['BITLY_TOKEN']
+    token = os.environ['BITLY_TOKEN']
     try:
         input_link = input('Enter bitlink or any url\n')
-        if is_bitlink(my_token, input_link):
-            print('The number of clicks is ', count_clicks(my_token, input_link))
+        if is_bitlink(token, input_link):
+            print('The number of clicks is ', count_clicks(token, input_link))
         else:
-            print('Bitlink for your url: ', shorten_link(my_token, input_link))
+            print('Bitlink for your url: ', shorten_link(token, input_link))
     except requests.exceptions.HTTPError:
         print('Entered url is not valid!')
